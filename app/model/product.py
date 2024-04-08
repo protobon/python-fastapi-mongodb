bd1ce3c38a1d212633cd762d872a4545e6368ee1
ayrton <ayrtoncoelhods@gmail.com>
2024-04-08 02:59:37 -0300

    environment

--- app/model/product.py
+++ app/model/product.py
@@ -1,10 +1,12 @@
 from mongoengine import (DynamicDocument, StringField, DateTimeField, IntField)
+from app.common.constants import EnvConstants as Env
+import os
 
 
 class Product(DynamicDocument):
     meta = {
         'collection': 'product',
-        'db_alias': 'warehouseDB'
+        'db_alias': os.getenv(Env.MONGODB.alias)
     }
 
     title = StringField(required=True, min_length=2, max_length=125)
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from loguru import logger
+import sys
+
 from app.common.config import Config
 from app.start import run
-import click
 
 
-@click.command()
-@click.option('--conf', default='config.yml', type=click.Path())
-def run_server(conf):
-    Config(path=conf)
-    run()
+def run_server():
+    try:
+        Config()
+        run()
+    except Exception:
+        logger.exception("Run Failed")
 
 
 if __name__ == "__main__":
+    logger.add(sys.stderr, format="{time} {level} {message}", level="INFO")
     run_server()
